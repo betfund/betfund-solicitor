4fbf11fe1a9dabce95bd0279f0d541f92e57826e
Mitchell Bregman <mitchbregs@gmail.com>
2020-04-03 02:11:17 -0400

    tests

--- betfund_solicitor/message.py
+++ betfund_solicitor/message.py
@@ -8,15 +8,15 @@ class Message:
 
     Attributes
     ----------
+    reason : str
+        Explanation in simple words for email. Can be a code in future.
+            For logging purposes.
     sender : str
         The email address that is sending the email.
     to : list
         The destination for this email.
     subject : str
         The subject of the message.
-    reply_to : list
-        If the recipient replies to the message, each reply-to address will
-        receive the reply.
     body_text : str
         The message body in plaintext.
     body_html : str
@@ -27,6 +27,9 @@ class Message:
         The recipients to place on the CC.
     bcc : list
         The recipients to place on the BCC.
+    reply_to : list
+        If the recipient replies to the message, each reply-to address will
+        receive the reply.
     charset : str
         The character set of the content.
     tags : list
@@ -34,33 +37,39 @@ class Message:
     """
     def __init__(
         self,
+        reason: str,
         sender: str,
         to: Union[str, List[str]],
         subject: str,
-        reply_to: Union[str, List[str]],
-        body_text: str = '',
-        body_html: str = '',
+        body_text: str,
+        body_html: str,
         attachments: Union[str, List[str]] = [],
         cc: Union[str, List[str]] = [],
         bcc: Union[str, List[str]] = [],
+        reply_to: Union[str, List[str]] = [],
         charset: str = "UTF-8",
         tags: List[Dict[str, str]] = []
     ):
+        self.reason = reason
         self.sender = sender
         self.to = to if isinstance(to, list) else [to]
         self.subject = subject
-        self.reply_to = reply_to if isinstance(reply_to, list) else [reply_to]
         self.body_text = body_text
         self.body_html = body_html
-        self.attachments = attachments if isinstance(attachments, list) else [attachments]
+        self.attachments = attachments if isinstance(attachments, list) \
+            else [attachments]
         self.cc = cc if isinstance(cc, list) else [cc]
         self.bcc = bcc if isinstance(bcc, list) else [bcc]
+        self.reply_to = reply_to if isinstance(reply_to, list) else [reply_to]
         self.charset = charset
         self.tags = tags
 
+    def __repr__(self):
+        return f"<Message `{self.reason}`>"
+
     @property
     def ses_send_email_payload(self):
-        """Property converting attributes to AWS SES configuration."""
+        """Property converting attributes to AWS SES `send_email` configuration."""
 
         # Payload kwargs for `boto3.client.send_email` method
         # See: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ses.html#SES.Client.send_email
---
+++ tests/test_message.py
@@ -0,0 +1,60 @@
+"""Unit tests for `lines.snapshot` modules."""
+import json
+import pytest
+
+from jsonschema import validate
+from unittest import TestCase
+
+from betfund_solicitor import Message
+
+
+def rjson(filepath):
+    return json.loads(open(filepath).read())
+
+
+class TestBasicMessage(TestCase):
+    """Tests for `betund_solicitor.message.Message`."""
+
+    def setUp(self) -> None:
+
+        # instantiate `betfund_solicitor.message.Message` object
+        self.message = Message(
+            reason='test',
+            sender='mitchbregs@gmail.com',
+            to='mitchbregs@gmail.com',
+            subject='SubjectTest',
+            body_text='TextTest',
+            body_html=''
+        )
+
+        # register basic payload
+        self.ses_send_email_payload = rjson(
+            'tests/payloads/ses_send_email_payload_basic.json'
+        )
+
+        # register basic json schema
+        self.ses_send_email_schema = rjson(
+            'tests/schemas/ses_send_email_schema_basic.json'
+        )
+
+    def test_constructor(self):
+        """Unit test for `Message.__init__(...)` success."""
+        assert self.message.reason == 'test'
+        assert self.message.sender == 'mitchbregs@gmail.com'
+        assert self.message.to == ['mitchbregs@gmail.com']
+        assert self.message.subject == 'SubjectTest'
+        assert self.message.body_text == 'TextTest'
+        assert self.message.body_html == ''
+
+    def test_ses_send_email_payload(self):
+        """Unit test for `Message.__init__(...)` success."""
+        pass
+
+    def test_ses_send_email_schema(self):
+        """Unit test for `Message.ses_send_email_payload` success."""
+
+        # use `jsonschema.validate` to assert that payload has proper typing
+        validate(
+            instance=self.message.ses_send_email_payload,
+            schema=self.ses_send_email_schema
+        )
