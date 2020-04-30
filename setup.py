e1e247e0120dc47f7dfe20128a3efa8351f5e961
Mitchell Bregman <mitchbregs@gmail.com>
2020-04-30 12:16:19 -0400

    remove self requires

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     keywords="betfund",
     packages=find_packages(),
     install_requires=[
-        "betfund_logger @ git+https://github.com/betfund/betfund-logger.git",
+        "betfund_logger @ git+https://github.com/betfund/betfund-logger.git@0.0.2",
         "boto3",
         "jsonschema",
         "sendgrid"
